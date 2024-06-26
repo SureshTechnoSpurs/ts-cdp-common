99f4a322ae736b1010c6324300976929db9494a6
SureshTechnoSpurs <166610207+SureshTechnoSpurs@users.noreply.github.com>
2024-06-26 19:29:57 +0530

    Added few files for TS common

---
+++ src/TS.Common/TS.Common.PubSub/PubSubClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TS.Common.Authentication;
+
+namespace TS.Common.PubSub
+{
+    public class PubSubClient : IPubSubClient
+    {
+        private readonly IAuthenticationClient _authenticationClient;
+        public PubSubClient(IAuthenticationClient authenticationClient)
+        {
+            _authenticationClient = authenticationClient;
+        }
+    }
+}
---
+++ src/TS.Common/TS.Common.PubSub/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TS.Common.Authentication;
+
+namespace TS.Common.PubSub
+{
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Registers all required services for Azure Data Factory integration.
+        /// </summary>
+        public static IServiceCollection AddPubSubClient(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthenticationClient(configuration);
+            services.AddTransient<IPubSubClient, PubSubClient>();
+
+            return services;
+        }
+
+    }
+}
